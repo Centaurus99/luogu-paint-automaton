25726ea3ddc7b8de2ad3ccb64f9f9f0592ec9c0e
Centaurus99 <45268165+Centaurus99@users.noreply.github.com>
2019-01-01 02:58:53 +0800

    Update
    requests_get发生503错误后自动重做
    仅在对绘板成功进行修改之后重新获取绘板内容，减少通讯量
    将用户序列改为用户队列，每次仅检查队头元素是否能正常操作，操作成功后丢到队尾
    “nothing to do”之后进入维护模式（间隔一段时间检查）减少网络和系统资源占用，需要操作再时进入工作模式

--- painter.py
+++ painter.py
@@ -1,138 +1,185 @@
-import json, requests
+import json, requests, time
 
-delta_x = 100 # set to move the picture
-delta_y = 100 # real_x = gived_x + delta_x
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+#delta_x = 500 # set to move the picture
+#delta_y = 391 # real_x = gived_x + delta_x
+delta_x = 0
+delta_y = 0
 
 headers = {
-	'referer': 'https://www.luogu.org/paintBoard',
-	'User-agent': 'wxw_ak_ioi'
+        'referer': 'https://www.luogu.org/paintBoard',
+        'User-agent': 'wxw_ak_ioi'
 }
 
 cookies = { '__client_id': '', '_uid': '' }
 
-def get_real_color(c):
-	if len(c) > 1:
-		return int(c)
-	if ord(c) >= ord('a'):
-		return ord(c) - ord('a') + 10
-	else:
-		return ord(c) - ord('0')
-
-class info:
-	def __init__(self, x, y, c):
-		self.x = int(x) + delta_x
-		self.y = int(y) + delta_y
-		self.c = get_real_color(c)
-
 def requests_get(url):
-	while True:
-		try:
-			req = requests.get(url, verify=False,
-			headers=headers, cookies=cookies, timeout=10)
-		except:
-			pass
-		break
-	return req
+        while True:
+                try:
+                        req = requests.get(url, verify=False,headers=headers, cookies=cookies, timeout=10)
+                        break
+                except:
+                        pass
+        return req
 
 def requests_post(url, data=None):
-	while True:
-		try:
-			req = requests.post(url, verify=False,
-			headers=headers, cookies=cookies, data=data, timeout=10)
-		except:
-			pass
-		break
-	return req
+        while True:
+                try:
+                        req = requests.post(url, verify=False,headers=headers, cookies=cookies, data=data, timeout=10)
+                        break
+                except:
+                        pass
+        return req
+
+
+def get_real_color(c):
+        if len(c) > 1:
+                return int(c)
+        if ord(c) >= ord('a'):
+                return ord(c) - ord('a') + 10
+        else:
+                return ord(c) - ord('0')
+
+class info:
+        def __init__(self, x, y, c):
+                self.x = int(x) + delta_x
+                self.y = int(y) + delta_y
+                self.c = get_real_color(c)
+
+global flag
+global content
+def get_board2():
+        global content
+        global flag
+        if flag == 1:
+                request = requests_get('https://www.luogu.org/paintBoard/board')
+                with open('board.out', 'wb+') as file:
+                        file.write(request.content)
+                        file.close()
+                content = str(request.content).split('\\n')
+                print('--get request')
+                flag = 0
+                
+        board = []
+        for i in range(0, 800):
+                line = []
+                for j in range(0, 400):
+                        color = content[i][j]
+                        line.append(color)
+                        # print(i, j, point)
+                board.append(line)
+        return board
 
 def get_board():
-	request = requests_get('https://www.luogu.org/paintBoard/board')
-	with open('board.out', 'wb+') as file:
-		file.write(request.content)
-		file.close()
-	content = str(request.content).split('\\n')
-	board = []
-	for i in range(0, 800):
-		line = []
-		for j in range(0, 400):
-			color = content[i][j]
-			line.append(color)
-			# print(i, j, point)
-		board.append(line)
-	return board
+        nowboard = []
+        global flag
+        while True:
+                try:
+                        nowboard = get_board2()
+                        break
+                except:
+                        print('-----!!! request error !!!-----')
+                        flag = 1
+                        time.sleep(1)
+        return nowboard
 
+        
+global nowstatus
 def paint(x, y, color):
-	data = { 'x': x, 'y': y, 'color': color }
-	request = requests_post('https://www.luogu.org/paintBoard/paint', data)
-	status = json.loads(request.content)['status']
-	if status == 200:
-		print('[200] Success by', cookies['_uid'], '!')
-		return True
-	elif status == 401:
-		print('[401] Not login. (', cookies['_uid'], ')')
-	elif status == 500:
-		# print('[500] Please wait for trying again.')
-		pass
-	else:
-		print('[???] Unknown error.')
-	return False
+        data = { 'x': x, 'y': y, 'color': color }
+        request = requests_post('https://www.luogu.org/paintBoard/paint', data=data)
+        status = json.loads(request.content)['status']
+        global nowstatus
+        nowstatus = status
+        if status == 200:
+                print('[200] Success by', cookies['_uid'], '!')
+        elif status == 401:
+                print('[401] Not login.')
+        elif status == 500:
+                print('[500] Please wait for trying again.')
+        else:
+                print('[???] Unknown error.')
 
 def get_todo():
-	content = open('todo.list', 'r+').read()
-	todolist = []
-	for line in content.split('\n'):
-		if len(line.split(' ')) < 3:
-			continue
-		x, y, c = line.split(' ')
-		todolist.append(info(x, y, c))
-	return todolist
+        content = open('todo.list', 'r+').read()
+        todolist = []
+        for line in content.split('\n'):
+                if len(line.split(' ')) < 3:
+                        continue
+                x, y, c = line.split(' ')
+                todolist.append(info(x, y, c))
+        return todolist
 
-def get_cookies():
-	content = open('cookies.list', 'r+').read()
-	cookies_list = []
-	for line in content.split('\n'):
-		cookies = {}
-		if len(line.split(' ')) < 2:
-			continue
-		cookies['__client_id'], cookies['_uid'] = line.split(' ')
-		cookies_list.append(cookies)
-	return cookies_list
-
-def clear_todo(todolist):
-	board = get_board()
-	answer = []
-	for todo in todolist:
-		if get_real_color(board[todo.x][todo.y]) != todo.c:
-			answer.append(todo)
-	return answer
 
 def check(x, y, color):
-	board = get_board()
-	now_color = get_real_color(board[x][y])
-	if color == now_color:
-		return True
-	else:
-		return False
+        board = get_board()
+        now_color = get_real_color(board[x][y])
+        #print('now_color:',now_color)
+        #print('check (', todo.x, ',', todo.y, ') to', todo.c)
+        if color == now_color:
+                #print('True')
+                return True
+        else:
+                #print('False')
+                return False
 
-if __name__ == '__main__':
-	while True:
+def get_cookies():
+        content = open('cookies.list', 'r+').read()
+        cookies_list = []
+        for line in content.split('\n'):
+                cookies = {}
+                if len(line.split(' ')) < 2:
+                        continue
+                cookies['__client_id'], cookies['_uid'] = line.split(' ')
+                cookies_list.append(cookies)
+        return cookies_list
 
-		todolist = clear_todo(get_todo())
-		cookies_list = get_cookies()
-		if len(todolist):
-			print('length of todolist is', len(todolist))
-		else:
-			print('nothing to do.')
+def clear_todo(todolist):
+        board = get_board()
+        answer = []
+        for todo in todolist:
+                if get_real_color(board[todo.x][todo.y]) != todo.c:
+                        answer.append(todo)
+        return answer
 
-		for todo in todolist:
-			print('todo (', todo.x, ',', todo.y, ') to', todo.c)
-			try:
-				success = check(todo.x, todo.y, todo.c)
-				while not success:
-					# print('paint (', todo.x, ',', todo.y, ') to', todo.c)
-					for user in cookies_list:
-						cookies = user
-						success = paint(todo.x, todo.y, todo.c)
-						if success:
-							break
-			except:
-				print('error')
+user_list = get_cookies()
+SleepLim =0
+while True:
+        print('')
+        print('Start!!!')
+        print('Top user :',user_list[0]['_uid'])
+        global flag
+        flag = 1
+        todolist = clear_todo(get_todo())
+        if len(todolist):
+                print('length of todolist is', len(todolist))
+                SleepLim = 0
+        else:
+                print('nothing to do.')
+                SleepLim = 5
+        print('Now SleepLim :',SleepLim)
+        time.sleep(SleepLim)
+        for todo in todolist:
+                print('todo (', todo.x, ',', todo.y, ') to', todo.c)
+                try:
+                        while not check(todo.x, todo.y, todo.c):
+                                print('paint (', todo.x, ',', todo.y, ') to', todo.c)
+                                for userid in range(len(user_list)):
+                                        cookies = user_list[userid]
+                                        paint(todo.x, todo.y, todo.c)
+                                        #print('nowstatus :',nowstatus)
+                                        if nowstatus == 500:
+                                                break
+                                        if nowstatus == 200:
+                                                flag = 1
+                                                for j in range(userid,len(user_list)-1):
+                                                        temp=user_list[j]
+                                                        user_list[j]=user_list[j+1]
+                                                        user_list[j+1]=temp
+                                                break
+                                     #   if check(todo.x, todo.y, todo.c):
+                                     #           break
+                except:
+                        print('error')
